714bb2e85b26192bf4c5ac60a2a7506c24aaea2e
Jason Gustafson <jason@confluent.io>
2016-05-06 14:43:17 -0700

    remove uninteresting test case

--- tests/kafkatest/tests/connect/connect_distributed_test.py
+++ tests/kafkatest/tests/connect/connect_distributed_test.py
@@ -106,21 +106,6 @@ class ConnectDistributedTest(Test):
         status = self._connector_status(connector, node)
         return self._has_state(status, 'PAUSED') and self._all_tasks_have_state(status, task_count, 'PAUSED')
 
-    def test_basic_connector_status(self):
-        self.setup_services()
-        self.cc.set_configs(lambda node: self.render("connect-distributed.properties", node=node))
-        self.cc.start()
-
-        self.logger.info("Creating connectors")
-        self._start_connector("connect-file-source.properties")
-        connector = 'local-file-source'
-        task_count = 1
-        
-        # all nodes should see the transition to the RUNNING state
-        for node in self.cc.nodes:
-            wait_until(lambda: self.is_running(connector, task_count, node), timeout_sec=30,
-                       err_msg="Failed to see connector transition to the RUNNING state")
-
     def test_pause_and_resume(self):
         self.setup_services()
         self.cc.set_configs(lambda node: self.render("connect-distributed.properties", node=node))
