fe08a16cc79fd9c06e91020fe875be240f3e9fbd
Jason Gustafson <jason@confluent.io>
2017-05-21 16:54:16 -0700

    Increase NetworkClient logging and reenable one of the bounce tests

--- core/src/test/scala/integration/kafka/api/ConsumerBounceTest.scala
+++ core/src/test/scala/integration/kafka/api/ConsumerBounceTest.scala
@@ -287,7 +287,6 @@ class ConsumerBounceTest extends IntegrationTestHarness with Logging {
    * immediately if rebalance is in progress. If brokers are not available,
    * close should terminate immediately without sending leave group.
    */
-  @Ignore
   @Test
   def testCloseDuringRebalance() {
     val topic = "closetest"
